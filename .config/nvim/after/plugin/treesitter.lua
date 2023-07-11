cc03dd9c22c2e8bb2872e4d343e40de5a537b3fe
fayaz-modz <mdfayaz9963@gmail.com>
2023-07-11 15:43:35 +0530

    added auto zR

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -24,6 +24,22 @@ require 'nvim-treesitter.configs'.setup {
 
 local vim = vim
 local opt = vim.opt
-
+local api = vim.api
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+
+local M = {}
+-- function to create a list of commands and convert them to autocommands
+-------- This function is taken from https://github.com/norcalli/nvim_utils
+function M.nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        api.nvim_command('augroup '..group_name)
+        api.nvim_command('autocmd!')
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            api.nvim_command(command)
+        end
+        api.nvim_command('augroup END')
+    end
+end
