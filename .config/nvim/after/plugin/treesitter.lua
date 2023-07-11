908fb72ea9be19f0165cbeab10fe68c3b44fd6e4
fayaz-modz <mdfayaz9963@gmail.com>
2023-07-11 15:47:10 +0530

    fixed codefold open at start

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -30,8 +30,6 @@ opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 
 local M = {}
--- function to create a list of commands and convert them to autocommands
--------- This function is taken from https://github.com/norcalli/nvim_utils
 function M.nvim_create_augroups(definitions)
     for group_name, definition in pairs(definitions) do
         api.nvim_command('augroup '..group_name)
@@ -43,3 +41,12 @@ function M.nvim_create_augroups(definitions)
         api.nvim_command('augroup END')
     end
 end
+
+local autoCommands = {
+    -- other autocommands
+    open_folds = {
+        {"BufReadPost,FileReadPost", "*", "normal zR"}
+    }
+}
+
+M.nvim_create_augroups(autoCommands)
