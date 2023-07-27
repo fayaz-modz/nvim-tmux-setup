5d742d047a1301167b244f57d9c0f10f84a996f0
Fayaz Mohammad <mdfayaz9963@gmail.com>
2023-07-27 12:25:59 +0530

    ;q map to esc

--- .config/nvim/lua/fayaz/packer.lua
+++ .config/nvim/lua/fayaz/packer.lua
@@ -4,65 +4,71 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use 'navarasu/onedark.nvim'
+
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },       -- Required
+      {                                  -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.api.nvim_command, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' },       -- Optional
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },           -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },       -- Required
+      { 'L3MON4D3/LuaSnip' },           -- Required
     }
+  }
 
-    use 'navarasu/onedark.nvim'
+  use("folke/zen-mode.nvim")
+  use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' }
 
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons',       -- optional, for file icons
+    },
+    tag = 'nightly'                        -- optional, updated every week. (see issue #1193)
+  }
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {                  -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.api.nvim_command, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+  use({
+    "kylechui/nvim-surround",
+    tag = "*",     -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  })
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' }, -- Required
-        }
-    }
-
-    use("folke/zen-mode.nvim")
-    use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' }
-
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
-    }
-
-    use({
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    })
-
-    use({
-        "aserowy/tmux.nvim",
-        config = function() return require("tmux").setup() end
-    })
+  use({
+    "aserowy/tmux.nvim",
+    config = function() return require("tmux").setup() end
+  })
 end)
--- .config/nvim/lua/fayaz/remap.lua
+++ .config/nvim/lua/fayaz/remap.lua
@@ -13,12 +13,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
+-- set exit to normal mode
+vim.keymap.set("i", ";q", "<ESC>")
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
